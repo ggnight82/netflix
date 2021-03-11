bc0e670f919766437c48a7eaaa7b3cc645ca442e
ggnight82 <ggnight82@naver.com>
2021-03-12 01:10:16 +0900

    delete main

--- Netflix/urls.py
+++ Netflix/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/',include('main.urls')),
+    #path('main/',include('main.urls')),
     path('browse/',include('browse.urls')),
     path('account/',include('account.urls')),
     path('rest-auth/',include('rest_auth.urls')),
